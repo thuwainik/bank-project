39292d556ef95a958fdc390e30119fb1bb383fc4
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-10-17 18:13:59 +0300

    .img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div className="">
       <div className="">
-        <h1 className=""></h1>
-        <img src="" />
+        <h1 className="">DRAHEM Bank</h1>
+        <img src="https://www.flaticon.com/free-icon/payment_4565110?related_id=4565110" />
         <p className=""></p>
       </div>
     </div>
