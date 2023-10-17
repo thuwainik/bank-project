af2192bc622d6b47db63fc048300cd4627523c07
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-10-17 18:08:13 +0300

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
-    <div className="max-w-3xl text-center">
-      <h1 className="text-6xl font-bold mb-8"></h1>
-      <p className="text-2xl leading-relaxed text-left">
-        
-      </p>
+    <div className="">
+      <div className="">
+        <h1 className=""></h1>
+        <p className=""></p>
+      </div>
     </div>
-  </div>
-);
-  }
-  
+  );
+};
 
-    export default Home;
+export default Home;
