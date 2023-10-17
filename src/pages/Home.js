4cae512609e577c94b3a7d6c823e7bac61b7847f
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-10-17 18:06:21 +0300

    home

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+    <div className="max-w-3xl text-center">
+      <h1 className="text-6xl font-bold mb-8"></h1>
+      <p className="text-2xl leading-relaxed text-left">
+        
+      </p>
+    </div>
+  </div>
+);
+  }
+  
+
+    export default Home;
