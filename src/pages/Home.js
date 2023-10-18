7501262776fcbf9e36ae48d79834fa2db77bfca4
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-10-18 16:10:58 +0300

    in favicon there is an error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ import bankimg from "../img/payment.png";
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="">
-        <h1 className="">DRAHEM Bank</h1>
-        <img src={bankimg} />
-        <p className=""></p>
+    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+      <div className="max-w-3xl text-center">
+        <h1 className="text-6xl font-bold mb-8">DRAHEM Bank</h1>
+        <h4 className="text-6xl font-shadow mb-8">@DRM</h4>
+        <p className="text-2xl leading-relaxed text-left"></p>
       </div>
     </div>
   );
